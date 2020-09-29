de961f6ef17cbc28df6b02b26212182a851bec99
Grigory Entin <grigory.entin@gmail.com>
2020-09-29 06:48:43 +0200

    Switched to getting video media by path.

--- Sources/BlinkKit/Actions/BlinkController+GetVideo.swift
+++ Sources/BlinkKit/Actions/BlinkController+GetVideo.swift
@@ -9,20 +9,10 @@ extension BlinkController {
         let url: URL
     }
     
-    public func getVideo(mediaID: Int) -> AnyPublisher<VideoResponse, Error> {
+    public func getVideo(media: String) -> AnyPublisher<VideoResponse, Error> {
         loggedIn()
-            .flatMap { loginResponse in
-                BlinkDefaultAPI.getVideoEvents(accountID: loginResponse.accountID, since: .distantPast, page: 0)
-            }
-            .flatMap { (videoEvents: BlinkOpenAPI.VideoEvents) -> AnyPublisher<URL, Error> in
-                _ = x$(videoEvents)
-                guard let media = videoEvents.media.first(where: { $0.id == mediaID }) else {
-                    enum Error: Swift.Error {
-                        case mediaNotFound(id: Int)
-                    }
-                    return Fail(error: Error.mediaNotFound(id: mediaID)).eraseToAnyPublisher()
-                }
-                return BlinkDefaultAPI.getVideo(media: x$(media.media))
+            .flatMap { _ in
+                BlinkDefaultAPI.getVideo(media: media)
             }
             .map { url in
                 VideoResponse(url: x$(url))
