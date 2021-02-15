8497236750e28a58c0822195260dc05f3608c622
Grigory Entin <grigory.entin@gmail.com>
2021-02-16 00:19:51 +0100

    Updated for new BlinkOpenAPI.

--- Sources/BlinkKit/Integrations/CustomURLSessionDecodableRequestBuilder.swift
+++ Sources/BlinkKit/Integrations/CustomURLSessionDecodableRequestBuilder.swift
@@ -8,8 +8,8 @@ import GETracing
 
 class CustomURLSessionDecodableRequestBuilder<T>: URLSessionDecodableRequestBuilder<T> where T: Decodable {
     
-    required init(method: String, URLString: String, parameters: [String: Any]?, isBody: Bool, headers: [String: String] = [:]) {
-        super.init(method: method, URLString: URLString, parameters: parameters, isBody: isBody, headers: headers)
+    required init(method: String, URLString: String, parameters: [String: Any]?, headers: [String: String] = [:]) {
+        super.init(method: method, URLString: URLString, parameters: parameters, headers: headers)
         taskDidReceiveChallenge = { (_, _, _) in
             (.performDefaultHandling, nil)
         }
--- Sources/BlinkKit/Integrations/CustomURLSessionRequestBuilder.swift
+++ Sources/BlinkKit/Integrations/CustomURLSessionRequestBuilder.swift
@@ -8,8 +8,8 @@ import GETracing
 
 class CustomURLSessionRequestBuilder<T>: URLSessionRequestBuilder<T> {
     
-    required public init(method: String, URLString: String, parameters: [String: Any]?, isBody: Bool, headers: [String: String] = [:]) {
-        super.init(method: method, URLString: URLString, parameters: parameters, isBody: isBody, headers: headers)
+    required init(method: String, URLString: String, parameters: [String: Any]?, headers: [String: String] = [:]) {
+        super.init(method: method, URLString: URLString, parameters: parameters, headers: headers)
         taskDidReceiveChallenge = { (_, _, _) in
             (.performDefaultHandling, nil)
         }
